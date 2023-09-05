36efc2d48bc7646b34e1541cea6ba57caab5896a
Vinicius Lima <viniuciusyl@hotmail.com>
2023-09-05 12:40:32 -0300

    v1.5

---
+++ projeto terça/login/login.js
@@ -0,0 +1,8 @@
+function Cadastro() {
+    if ($('#nome').val().length < 0) {
+        alert("Preencha todos os campos!"); 
+    } 
+    else {
+        alert("Cadastro efetuado com sucesso!"); 
+    }
+}
