8e33d37769b7ce586c7e30a72ece0f2fd599245a
Vinicius Lima <viniuciusyl@hotmail.com>
2023-09-06 19:13:23 -0300

    v2.2

--- projeto terça/paginaCarros/script.js
+++ projeto terça/paginaCarros/script.js
@@ -2,8 +2,8 @@ function comprarCarro(modelo, preco) {
     alert(`Você comprou um ${modelo} por $${preco}`);
 }
 
-var img1 = "img/Jaguar/f-pace_1.jpg";
-var img2 = "img/Jaguar/f-pace_2.jpg";
+var img1 = "../img/Jaguar/f-pace_1.jpg";
+var img2 = "../img/Jaguar/f-pace_2.jpg";
 
 function trocar1(){								 
     element=document.getElementById('figura1').src= img1;
@@ -12,8 +12,8 @@ function trocar1(){
     img2 = aux;
 }
 
-var img3 = "img/BMW/BMW-320I_1.jpg";
-var img4 = "img/BMW/BMW-320I_2.jpg";
+var img3 = "../img/BMW/BMW-320I_1.jpg";
+var img4 = "../img/BMW/BMW-320I_2.jpg";
    
 function trocar2(){								 
     element=document.getElementById('figura2').src= img3;
