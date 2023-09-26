1c3cbaebf6cfdb9afcc945f4ca40ea2df9009e23
Vinicius Lima <viniuciusyl@hotmail.com>
2023-09-26 17:21:20 -0300

    v2.8

--- projeto terça/paginaAnuncio/script.js
+++ projeto terça/paginaAnuncio/script.js
@@ -1,17 +1,43 @@
-var db = openDatabase("Concessionaria", "1.0", "Teste", 4048);
-db.transaction(function(criar){
-    criar.executeSql("CREATE TABLE concessionaria (marca TEXT NOT NULL PRIMARY KEY, modelo TEXT NOT NULL, motor TEXT NOT NULL, combustivel TEXT NOT NULL, cambio TEXT NOT NULL, direcao TEXT NOT NULL)");
-});
+window.addEventListener('load', carregado);
 
-function salvar(){
-    var marca = document.getElementById('marca').value;
-    var modelo = document.getElementById('modelo').value;
-    var motor = document.getElementById('motor').value;
-    var combustivel = document.getElementById('combustivel').value;
-    var cambio = document.getElementById('cambio').value;
-    var direcao = document.getElementById('direçao').value;
-    db.transaction(function(armazenar){
-        armazenar.executeSql('INSERT INTO concessionaria (marca,modelo,motor,combustivel,cambio,direcao) VALUES (?,?,?,?,?,?)',[marca,modelo,motor,combustivel,cambio,direcao]);
-    });
-}
+        var db = openDatabase("Concessionaria", "1.0", "Teste", 4048);
+
+        db.transaction(function(tx) {
+            tx.executeSql("DROP TABLE IF EXISTS concessionaria");
+        });
+
+        db.transaction(function(tx) {
+            tx.executeSql("CREATE TABLE IF NOT EXISTS concessionaria (id INTEGER PRIMARY KEY,\
+                                                                        marca TEXT,\
+                                                                        modelo TEXT,\
+                                                                        motor TEXT,\
+                                                                        combustivel TEXT,\
+                                                                        cambio TEXT,\
+                                                                        direcao TEXT)");
+        });
+
+        function carregado() {
+            document.getElementById('btnCadastrar').addEventListener('click', salvar);
+        }
+
+        function salvar() {
+            var marca = document.getElementById('marca').value;
+            var modelo = document.getElementById('modelo').value;
+            var motor = document.getElementById('motor').value;
+            var combustivel = document.getElementById('combustivel').value;
+            var cambio = document.getElementById('cambio').value;
+            var direcao = document.getElementById('direcao').value;
+
+            db.transaction(function(tx) {
+                tx.executeSql('INSERT INTO concessionaria (marca, modelo, motor, combustivel, cambio, direcao) VALUES (?, ?, ?, ?, ?, ?)', [marca, modelo, motor, combustivel, cambio, direcao]);
+            });
+
+            // Limpar campos de entrada após inserir dados
+            document.getElementById('marca').value = '';
+            document.getElementById('modelo').value = '';
+            document.getElementById('motor').value = '';
+            document.getElementById('combustivel').value = '';
+            document.getElementById('cambio').value = '';
+            document.getElementById('direcao').value = '';
+        }
 
--- projeto terça/paginaCarros/script.js
+++ projeto terça/paginaCarros/script.js
@@ -2,23 +2,87 @@ function comprarCarro(modelo, preco) {
     alert(`Você comprou um ${modelo} por $${preco}`);
 }
 
-var img1 = "../img/Jaguar/f-pace_1.jpg";
+//JAGUAR
+var img1 = "../img/Jaguar/jaguar-xf_2.webp";
 var img2 = "../img/Jaguar/f-pace_2.jpg";
+var img3 = "../img/Jaguar/i-pace_2.jpg";
+var img4 = "../img/Jaguar/jaguar-xe_2.webp";
+var img5 = "../img/Jaguar/f-type_2.webp";
 
 function trocar1(){								 
     element=document.getElementById('figura1').src= img1;
     let aux = img1;
-    img1 = img2;
-    img2 = aux;
+    img1 = img2
+    img2 = img3
+    img3 = img4
+    img4 = img5
+    img5 = aux;
 }
 
-var img3 = "../img/BMW/BMW-320I_1.jpg";
-var img4 = "../img/BMW/BMW-320I_2.jpg";
-   
+//BMW
+var img6 = "../img/BMW/BMW-320I_2.jpg";
+var img7 = "../img/BMW/BMW-420I_2.jpg";
+var img8 = "../img/BMW/BMW-I7_1.jpeg";
+var img9 = "../img/BMW/BMW-M3_1.webp";
+var img10 = "../img/BMW/BMW-X6_2.jpg";
+
 function trocar2(){								 
-    element=document.getElementById('figura2').src= img3;
-    let aux = img3;
-    img3 = img4;
-    img4 = aux;
+    element=document.getElementById('figura2').src= img6;
+    let aux = img6;
+    img6 = img7
+    img7 = img8
+    img8 = img9
+    img9 = img10
+    img10 = aux;
 }
 
+//KOENIGSEGG
+var img11 = "../img/Koenigsegg/KOENIGSEGG-REGERA_2.jpg";
+var img12 = "../img/Koenigsegg/koenigsegg-gemera_1.jpeg";
+var img13 = "../img/Koenigsegg/KOENIGSEGG-CC850_2.webp";
+var img14 = "../img/Koenigsegg/KOENIGSEGG-RAGE_2.jpg";
+var img15 = "../img/Koenigsegg/KOENIGSEGG-JESKO_2.jpg";
+
+function trocar3(){								 
+    element=document.getElementById('figura3').src= img11;
+    let aux = img11;
+    img11 = img12
+    img12 = img13
+    img13 = img14
+    img14 = img15
+    img15 = aux;
+}
+
+//MERCEDES
+var img16 = "../img/Mercedes/MERCEDES-GLA-200_2.jpg";
+var img17 = "../img/Mercedes/MERCEDES-CLA35_2.jpg";
+var img18 = "../img/Mercedes/MERCEDES-EQC4_2.webp";
+var img19 = "../img/Mercedes/MERCEDES-C300_2.jpg";
+var img20 = "../img/Mercedes/MERCEDES-G63_2.webp";
+
+function trocar4(){								 
+    element=document.getElementById('figura4').src= img16;
+    let aux = img16;
+    img16 = img17
+    img17 = img18
+    img18 = img19
+    img19 = img20
+    img20 = aux;
+}
+
+//PORSCHE
+var img21 = "../img/Porsche/PORSCHE-PANAMERA2.jpg";
+var img22 = "../img/Porsche/PORSCHE_taycan2.jpg";
+var img23 = "../img/Porsche/PORSCHE-CAYENNE_2.jpg";
+var img24 = "../img/Porsche/PORSCHE-MACAN_2.webp";
+var img25 = "../img/Porsche/porsche-911_2.webp";
+
+function trocar5(){								 
+    element=document.getElementById('figura5').src= img21;
+    let aux = img21;
+    img21 = img22
+    img22 = img23
+    img23 = img24
+    img24 = img25
+    img25 = aux;
+}
